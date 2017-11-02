93f156800548e470792e5ad35d55ce441569b1f0
patport <patella.portuondo@mymona.uwi.edu>
2017-11-02 04:08:55 +0000

    exercise4

--- definition.js
+++ definition.js
@@ -9,4 +9,23 @@ $(document).ready(function (){
             }
         })
     })
+    
+    $("#searchall").click(function(){
+        $.ajax({
+            type:"GET",
+            url:"request.php",
+            data: { q: "&all=true"},
+            success: function(data){
+                $("#result").html("");
+                $("#result").append("<ol></ol>");
+                $(data).find("definition").each(function(){
+                    var list = "<h3>"+$(this).find("name").text()+"</h3>";
+                    list += "<p>"+$(this).find("meaning").text()+"</p>";
+                    list += "<p>"+"-"+$(this).find("author").text()+"</p>";
+                    $("#result ol").append("<li>"+list+"</li>");
+                })
+                
+            }
+        })
+    })
 })
--- request.php
+++ request.php
@@ -14,5 +14,49 @@ $definition = [
     "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
 ];
 
+$xmldata = '<?xml version="1.0" encoding="UTF-8"?>
+<entries>
+    <definition>
+        <name>definition</name>
+        <author>Pam</author>
+        <meaning>A statement of the exact meaning of a word, especially in a dictionary.</meaning>
+    </definition>
+    <definition>
+        <name>bar</name>
+        <author>Pam</author>
+        <meaning>A place that sells alcholic beverages</meaning>
+    </definition>
+    <definition>
+        <name>ajax</name>
+        <author>Pam</author>
+        <meaning>Technique which involves the use of javascript and xml (or JSON)</meaning>
+    </definition>
+    <definition>
+        <name>html</name>
+        <author>Pam</author>
+        <meaning>The standard markup language for creating web pages and web applications.</meaning>
+    </definition>
+    <definition>
+        <name>css</name>
+        <author>Pam</author>
+        <meaning>A style sheet language used for describing the presentation of a document written in a markup language.</meaning>
+    </definition>
+    <definition>
+        <name>javascript</name>
+        <author>Pam</author>
+        <meaning>A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.</meaning>
+    </definition>
+    <definition>
+        <name>php</name>
+        <author>Pam</author>
+        <meaning>A server-side scripting language, and a powerful tool for making dynamic and interactive websites</meaning>
+    </definition>
+</entries>';
+
+if($query=="&all=true")
+{header('Content-Type: text/xml');
+$xmlOutput = new SimpleXMLElement($xmldata);
+echo $xmlOutput->asXML();}
+else{
 print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>";
+print "<p>" . $definition[$query] . "</p>";}
